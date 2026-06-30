44941972a491a602beedd1ad96d4fc68676fbf99
Ayomide <ayoms947@gmail.com>
2026-06-30 15:46:50 +0100

    v0.1.1

--- landing_page/featured-projects/featured-projects-info.js
+++ landing_page/featured-projects/featured-projects-info.js
@@ -1,37 +1,37 @@
 export const featuredProjectsInfo = [
   {
-    Image: "./featured-projects/images/circuit-image.jpg",
+    Image: "../universal-components/images/circuit-image.jpg",
     Title: "Arduino Security Alarm System",
     Description:
       "Button Triggered alarm using motor, buzzer, and LED status system with secure feedback",
-    Tags: ["Arduino", "C++", "Electronics"],
+    Tags: ["Arduino", "C++", "Electronics", "Git & Github"],
     projectLink: "https://htmlcolorcodes.com/colors/neon-green/",
   },
 
   {
-    Image: "./featured-projects/images/circuit-image.jpg",
+    Image: "../universal-components/images/circuit-image.jpg",
     Title: "Arduino Security Alarm System",
     Description:
       "Button Triggered alarm using motor, buzzer, and LED status system with secure feedback",
-    Tags: ["Arduino", "C++", "Electronics"],
+    Tags: ["Arduino", "C++", "Electronics", "Git & Github"],
     projectLink: "https://htmlcolorcodes.com/colors/neon-green/",
   },
 
   {
-    Image: "./featured-projects/images/circuit-image.jpg",
+    Image: "../universal-components/images/circuit-image.jpg",
     Title: "Arduino Security Alarm System",
     Description:
       "Button Triggered alarm using motor, buzzer, and LED status system with secure feedback",
-    Tags: ["Arduino", "C++", "Electronics"],
+    Tags: ["Arduino", "C++", "Electronics", "Git & Github"],
     projectLink: "https://htmlcolorcodes.com/colors/neon-green/",
   },
 
   {
-    Image: "./featured-projects/images/circuit-image.jpg",
+    Image: "../universal-components/images/circuit-image.jpg",
     Title: "Arduino Security Alarm System",
     Description:
       "Button Triggered alarm using motor, buzzer, and LED status system with secure feedback",
-    Tags: ["Arduino", "C++", "Electronics"],
+    Tags: ["Arduino", "C++", "Electronics", "Git & Github"],
     projectLink: "https://htmlcolorcodes.com/colors/neon-green/",
   },
 ];
