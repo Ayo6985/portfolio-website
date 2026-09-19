0f2c54abbbcdf8299794580e2cf749fa5790e10a
Ayomide Adetiloye <Ayomide.Adetiloye@outlook.com>
2026-09-19 09:52:59 +0100

    change in project infor jeatured js

--- landing_page/featured-projects/featured-projects-info.js
+++ landing_page/featured-projects/featured-projects-info.js
@@ -1,7 +1,7 @@
 export const featuredProjectsInfo = [
   {
     Id: 0,
-    Image: "../universal-components/images/circuit-image.jpg",
+    Image: "universal-components/images/circuit-image.jpg",
     Title: "Arduino Security Alarm System",
     Description:
       "Button Triggered alarm using motor, buzzer, and LED status system with secure feedback",
@@ -10,7 +10,7 @@ export const featuredProjectsInfo = [
 
   {
     Id: 1,
-    Image: "../universal-components/images/circuit-image.jpg",
+    Image: "universal-components/images/circuit-image.jpg",
     Title: "Weight Sensing Glove",
     Description:
       "Glove utlising force sensitive resisors to calibrate the force applied on objects by the wearer",
